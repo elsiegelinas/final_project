4bf878a34c4837aab263e8b69a42248b7f96f81d
Raghu Betina <raghu@firstdraft.com>
2018-03-13 06:33:47 +0000

    Direct association between users and taggings

--- app/models/tagging.rb
+++ app/models/tagging.rb
@@ -1,6 +1,9 @@
 class Tagging < ApplicationRecord
   # Direct associations
 
+  belongs_to :user,
+             :required => false
+
   # Indirect associations
 
   # Validations
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :taggings,
+             :dependent => :nullify
+
   has_many   :user_reviews,
              :dependent => :destroy
 
