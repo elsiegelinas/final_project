2150e565915ef478e3d8f691786e7e0cab4c2633
Raghu Betina <raghu@firstdraft.com>
2018-03-13 06:33:55 +0000

    Direct association between cuisines and restaurants

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,9 @@
 class Restaurant < ApplicationRecord
   # Direct associations
 
+  belongs_to :cuisine,
+             :counter_cache => true
+
   belongs_to :neighborhood,
              :counter_cache => true
 
